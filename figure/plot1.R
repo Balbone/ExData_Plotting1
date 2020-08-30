2dac2a3dd6a8f943fdf6acfd0f3ce569b85df862
Balbone <49312361+Balbone@users.noreply.github.com>
2020-08-30 10:39:45 +0200

    Create plot1.R

---
+++ figure/plot1.R
@@ -0,0 +1,33 @@
+rm(list=ls())
+getwd()
+
+#unzip
+zipfile<- "~/Downloads/extemp_temp_household_power_consumption.zip"
+outDir<-"~/Desktop/programmingW1"
+unzip(zipfile,exdir=outDir)
+
+#load file
+temp <- read.table("~/Desktop/programmingW1/household_power_consumption.txt", skip=1,sep=";")
+View(temp)
+
+#names taken from the programming assignment page 
+#https://www.coursera.org/learn/exploratory-temp-analysis/peer/ylVFo/course-project-1
+names(temp) <- c("Date","Time","Global_active_power","Global_reactive_power",
+                  "Voltage","Global_intensity","Sub_metering_1","Sub_metering_2",
+                  "Sub_metering_3")
+View(temp)
+dim(temp)
+
+#from https://www.coursera.org/learn/exploratory-temp-analysis/peer/ylVFo/course-project-1
+#"We will only be using temp from the dates 2007-02-01 and 2007-02-02"
+data <- subset(temp, temp$Date=="1/2/2007" | temp$Date =="2/2/2007")
+View(data)
+dim(data)
+
+#first plot
+png("~/Desktop/programmingW1/plot1.png", width=480, height=480)
+
+hist(as.numeric(as.character(data$Global_active_power)),col="red",
+     main="Global Active Power",xlab="Global Active Power(kilowatts)")
+
+dev.off()
